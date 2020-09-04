9926e166d897bc52e60859f38ae29f4ed1d7e3dc
dayesouza <day.carol18@gmail.com>
2020-09-04 00:05:59 -0300

    initial structure with material icon installed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Buttons</h1>
       </header>
+      <div>
+        <Button />
+      </div>
+      <footer>
+        <h1>Day</h1>
+      </footer>
     </div>
   );
 }
